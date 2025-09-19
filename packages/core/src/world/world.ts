e0c113aac7005b78076219c7f133a7aba6e9231d
Palm <christopher.k.izatt@gmail.com>
2025-09-18 20:09:59 -0400

    Revert "Update world.ts"
    This reverts commit 45dda667146145a1d99cb17da2037c8f3102a97b.

--- packages/core/src/world/world.ts
+++ packages/core/src/world/world.ts
@@ -50,7 +50,7 @@ class World extends Emitter<WorldEventSignals> {
   /**
    * The identifier of the world.
    */
-  public readonly identifier: string;
+  public identifier: string;
 
   /**
    * The provider of the world.
