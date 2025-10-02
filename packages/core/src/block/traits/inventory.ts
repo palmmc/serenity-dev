636f223fdfae38f79dd6bc80b1a8b512ab631f78
Palm <christopher.k.izatt@gmail.com>
2025-10-02 17:42:26 -0400

    World method fixes
    Fix container as well

--- packages/core/src/block/traits/inventory.ts
+++ packages/core/src/block/traits/inventory.ts
@@ -48,19 +48,20 @@ class BlockInventoryTrait extends BlockTrait {
     );
   }
 
-  public onInteract({ cancel, origin }: BlockInteractionOptions): void {
+  public onInteract({ cancel, origin }: BlockInteractionOptions): boolean {
     // Check if the block interaction has been cancelled
-    if (cancel || !origin) return;
+    if (cancel || !origin) return false;
 
     // Check if the player is sneaking
     if (
       origin.isSneaking ||
       !origin.abilities.getAbility(AbilityIndex.OpenContainers)
     )
-      return;
+      return false;
 
     // Show the container to the player
     this.container.show(origin);
+    return false;
   }
 
   public onBreak(options?: BlockDestroyOptions): void {
